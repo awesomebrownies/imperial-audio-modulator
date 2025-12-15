8d1df067377de27807a0c434b638acc50cf24533
awesomebrownies <evan.n.brown25@gmail.com>
2025-12-15 11:43:51 -0700

    Add per second device polling

--- backend/imperial-audio-modulator.go
+++ backend/imperial-audio-modulator.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os/exec"
+	"time"
 
 	"github.com/sstallion/go-hid"
 )
@@ -22,69 +23,77 @@ func main() {
 	}
 	defer hid.Exit()
 
-	var targetDevice *hid.DeviceInfo
-	err := hid.Enumerate(VID, PID, func(info *hid.DeviceInfo) error {
-		fmt.Printf("%s: ID %04x:%04x Usage:%04x UsagePage:%04x %s %s\n",
-			info.Path,
-			info.VendorID,
-			info.ProductID,
-			info.Usage,
-			info.UsagePage,
-			info.MfrStr,
-			info.ProductStr)
-
-		// Find the device with our custom usage page
-		if info.UsagePage == USAGE_PAGE && info.Usage == USAGE {
-			targetDevice = info
-		}
-		return nil
-	})
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if targetDevice == nil {
-		log.Fatal("Toggle device not found - make sure boot.py is configured and device is replugged")
-	}
-
-	dev, err := hid.OpenPath(targetDevice.Path)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer dev.Close()
-
-	fmt.Println("Connected to toggle device")
-
-	var lastState byte = 0xFF
 	for {
-		buf := make([]byte, 2) // [reportID, data]
-		n, err := dev.Read(buf)
+		var targetDevice *hid.DeviceInfo
+
+		err := hid.Enumerate(VID, PID, func(info *hid.DeviceInfo) error {
+			fmt.Printf("%s: ID %04x:%04x Usage:%04x UsagePage:%04x %s %s\n",
+				info.Path,
+				info.VendorID,
+				info.ProductID,
+				info.Usage,
+				info.UsagePage,
+				info.MfrStr,
+				info.ProductStr)
+
+			if info.UsagePage == USAGE_PAGE && info.Usage == USAGE {
+				targetDevice = info
+			}
+			return nil
+		})
+
 		if err != nil {
-			log.Fatal(err)
-		}
-		if n < 2 {
+			log.Println("HID enumerate error:", err)
+			time.Sleep(time.Second)
 			continue
 		}
 
-		if buf[0] != TOGGLE_REPORT {
+		if targetDevice == nil {
+			time.Sleep(time.Second)
 			continue
 		}
 
-		state := buf[1]
-		switch state {
-		case 0b10000000:
-			fmt.Println("Mute Toggle Pressed")
-		case 0b11000001: //volume up
-			volume(lastState, "+")
-		case 0b11000000: //volume down
-			volume(lastState, "-")
-		default:
-			if state != lastState {
-				handleToggleChange(lastState, state)
-				lastState = state
+		dev, err := hid.OpenPath(targetDevice.Path)
+		if err != nil {
+			log.Println("Failed to open device:", err)
+			time.Sleep(time.Second)
+			continue
+		}
+
+		fmt.Println("Connected to toggle device")
+
+		lastState := byte(0xFF)
+
+		for {
+			buf := make([]byte, 2)
+			n, err := dev.Read(buf)
+			if err != nil {
+				fmt.Println("Device disconnected")
+				dev.Close()
+				break // go back to enumeration loop
+			}
+
+			if n < 2 || buf[0] != TOGGLE_REPORT {
+				continue
+			}
+
+			state := buf[1]
+			switch state {
+			case 0b10000000:
+				fmt.Println("Mute Toggle Pressed")
+			case 0b11000001:
+				volume(lastState, "+")
+			case 0b11000000:
+				volume(lastState, "-")
+			default:
+				if state != lastState {
+					handleToggleChange(lastState, state)
+					lastState = state
+				}
 			}
 		}
+
+		time.Sleep(time.Second)
 	}
 }
 
